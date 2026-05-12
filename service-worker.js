192e5c2124958556b91049f713cb18adf588b308
Aurelia <279377517+aureliaspirit@users.noreply.github.com>
2026-05-12 13:49:26 -0400

    v1.9.17

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
-﻿const CACHE_NAME = "heartbox-pwa-v1-9-16";
+﻿const CACHE_NAME = "heartbox-pwa-v1-9-17";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=1.9.16",
-  "./app.js?v=1.9.16",
-  "./v1916-patch.js?v=1.9.16",
-  "./manifest.json?v=1.9.16",
+  "./style.css?v=1.9.17",
+  "./app.js?v=1.9.17",
+  "./manifest.json?v=1.9.17",
   "./icons/icon-120.png",
   "./icons/icon-152.png",
   "./icons/icon-167.png",
@@ -90,3 +89,4 @@ self.addEventListener("fetch", (event) => {
     );
   }
 });
+
